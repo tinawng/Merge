162189fe69f473235029bcb8e11b2b21d863efff
Tina Wang <tina@tina.cafe>
2022-01-13 17:02:46 +0100

    🗑️ Cleanup

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,6 @@
 <template>
   <div class="layout__container">
     <Nuxt />
-    <!-- <section class="merge_history">
-      <span class="h2">Latest Merges</span>
-      <ul class="mt-8">
-        <li v-for="(token, i) in latest_merges" :key="token.id">
-          <div class="pill bg-gray" @click="openModal(token)">m({{token.mass}}) #{{token.id}}</div>
-          <icon variant="circle" class="w-1.5 animate-ping-slow" :style="`--delay: ${0.0+i/2}s`" />
-          <icon variant="circle" class="w-1.5 animate-ping-slow" :style="`--delay: ${0.4+i/2}s`" />
-          <div class="flex text-sm tracking-wider">
-            <div class="pt-1">{{token.sale_price.toFixed(4)}}</div>
-            <icon class="w-5 text-gray" />
-          </div>
-          <icon variant="circle" class="w-2 animate-ping-slow text-blue" :style="`--delay: ${0.8+i/2}s`" />
-          <icon variant="circle" class="w-2.5 animate-ping-slow text-blue" :style="`--delay: ${1.0+i/2}s`" />
-          <div class="pill bg-blue" @click="openModal(token.merged_to)">m(21) #{{token.merged_to}}</div>
-        </li>
-      </ul>
-    </section> -->
 
     <sections-merges />
     <sections-leaderboard />
