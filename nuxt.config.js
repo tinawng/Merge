c2f66fb822b6f9048049005b07f100b75f6c318b
Tina Wang <tina@tina.cafe>
2022-02-03 20:54:24 +0100

    🐛 Fix preview image name

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,8 +18,8 @@ export default {
       { hid: 'og:description', name: 'og:description', property: 'og:description', content: 'Metadata Dashboard' },
       { hid: 'og:url', name: 'og:url', property: 'og:url', content: 'https://merge.tina.cafe/' },
       { hid: 'twitter:card', name: 'twitter:card', property: 'twitter:card', content: 'summary_large_image' },
-      { hid: 'twitter:image', name: 'twitter:image', property: 'twitter:image', content: '/preview-large.jpg' },
-      { hid: 'og:image', name: 'og:image', property: 'og:image', content: '/preview-large.jpg' },
+      { hid: 'twitter:image', name: 'twitter:image', property: 'twitter:image', content: '/preview.jpg' },
+      { hid: 'og:image', name: 'og:image', property: 'og:image', content: '/preview.jpg' },
       { hid: 'og:image:width', name: 'og:image:width', property: 'og:image:width', content: '630' },
       { hid: 'og:image:height', name: 'og:image:height', property: 'og:image:height', content: '630' }
     ],
